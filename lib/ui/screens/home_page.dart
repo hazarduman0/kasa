a53362f77b1f3d5f42671377f576f6721b5f823a
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-03 23:17:06 +0300

    Update home_page.dart

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -6,13 +6,11 @@ import 'package:kasa/core/constrants/app_colors.dart';
 import 'package:kasa/core/constrants/app_keys.dart';
 import 'package:kasa/core/constrants/app_keys_textstyle.dart';
 import 'package:kasa/ui/widgets/home/activity_timeline.dart';
+import 'package:kasa/ui/widgets/home/bottom_sheet_widget.dart';
 import 'package:kasa/ui/widgets/home/month_comparison_card.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
-
-  final BottomSheetController sheetController = Get.find();
-  final CategoryController categoryController = Get.find();
 
   @override
   Widget build(BuildContext context) {
@@ -144,120 +142,7 @@ class HomePage extends StatelessWidget {
   }
 
   Future<dynamic> _getBottomSheet() {
-    return Get.bottomSheet(elevation: 0.0, isScrollControlled: true,
-        GetBuilder<BottomSheetController>(builder: (sheet) {
-      return Container(
-        height: Get.height * 0.8,
-        width: Get.width,
-        decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15.0),
-                topRight: Radius.circular(15.0))),
-        child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: Get.width * 0.03),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _bottomSheetCancelButton(),
-                SizedBox(height: Get.height * 0.05),
-                _buttonRow(),
-                SizedBox(height: Get.height * 0.03),
-                SizedBox(
-                    height: Get.height * 0.1,
-                    width: Get.width,
-                    child: GetBuilder<CategoryController>(builder: (category) {
-                      return DropdownButtonFormField(
-                        hint: const Text('Kategori Seçin'),
-                        //value: 'ali',
-                         dropdownColor: Colors.white,
-                        items: generateDropDownMenuItemList(),
-                        onChanged: (value) {
-                          category.selectCategory(value);
-                        },
-                      );
-                    })),
-              ],
-            )),
-      );
-    }));
-  }
-
-  List<DropdownMenuItem<String>> generateDropDownMenuItemList() {
-    List<DropdownMenuItem<String>> dropDownMenuItems = [];
-    if (sheetController.isRemove) {
-      for (var item in categoryController.expenseList) {
-        dropDownMenuItems.add(
-            DropdownMenuItem(value: item.category, child: Center(child: Text(item.category))));
-      }
-    } else {
-      for (var item in categoryController.incomeList) {
-        dropDownMenuItems.add(
-            DropdownMenuItem(value: item.category, child: Center(child: Text(item.category))));
-      }
-    }
-    return dropDownMenuItems;
-  }
-
-  Padding _buttonRow() {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: Get.width * 0.01),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          _fixedButton(sheetController.isRemove
-              ? AppKeys.fixedExpense
-              : AppKeys.fixedIncome),
-          _extraButton(
-              sheetController.isRemove ? AppKeys.unexpected : AppKeys.extra)
-        ],
-      ),
-    );
-  }
-
-  SizedBox _extraButton(String name) {
-    return SizedBox(
-      width: Get.width * 0.43,
-      child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              elevation: 0.0,
-              primary: !sheetController.isFixedChoosen
-                  ? AppColors.enchantingSapphire
-                  : AppColors.nieblaAzul),
-          onPressed: () {
-            sheetController.extraButtonFunc();
-          },
-          child: Text(name, style: AppKeysTextStyle.buttonTextStyle)),
-    );
-  }
-
-  SizedBox _fixedButton(String name) {
-    return SizedBox(
-      width: Get.width * 0.43,
-      child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              elevation: 0.0,
-              primary: sheetController.isFixedChoosen
-                  ? AppColors.enchantingSapphire
-                  : AppColors.nieblaAzul),
-          onPressed: () {
-            sheetController.fixedButtonFunc();
-          },
-          child: Text(name, style: AppKeysTextStyle.buttonTextStyle)),
-    );
-  }
-
-  Align _bottomSheetCancelButton() {
-    return Align(
-        alignment: Alignment.topRight,
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: GestureDetector(
-              onTap: () {
-                Get.back();
-              },
-              child: Text(AppKeys.cancel,
-                  style: AppKeysTextStyle.cancelTextStyle)),
-        ));
+    return Get.bottomSheet(
+        elevation: 0.0, isScrollControlled: true, BottomSheetWidget());
   }
 }
