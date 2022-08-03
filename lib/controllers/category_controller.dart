3651a761366651742a57a0075eebe361d1d3a372
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-03 23:17:01 +0300

    Update category_controller.dart

--- lib/controllers/category_controller.dart
+++ lib/controllers/category_controller.dart
@@ -8,19 +8,19 @@ class CategoryController extends GetxController {
 
   // final RxList<Category> _expenseCategoryList = <Category>[].obs;
   // final RxList<Category> _incomeCategoryList = <Category>[].obs;
-  // final RxList<String> _expenseCategoryList = <String>[].obs;
-  // final RxList<String> _incomeCategoryList = <String>[].obs;
+  final RxList<String> _expenseCategoryList = <String>[].obs;
+  final RxList<String> _incomeCategoryList = <String>[].obs;
   final RxList<Category> _categoryList = <Category>[].obs;
   final RxString _selectedCategory = ''.obs;
 
   String get selectedCategory => _selectedCategory.value;
   List<Category> get categoryList => _categoryList;
-  List<Category> get incomeList =>
-      _categoryList.where((category) => category.isIncome).toList();
-  List<Category> get expenseList =>
-      _categoryList.where((category) => !category.isIncome).toList();
-  // List<String> get incomeList => _incomeCategoryList;
-  // List<String> get expenseList => _expenseCategoryList;
+  // List<Category> get incomeList =>
+  //     _categoryList.where((category) => category.isIncome).toList();
+  // List<Category> get expenseList =>
+  //     _categoryList.where((category) => !category.isIncome).toList();
+  List<String> get incomeList => _incomeCategoryList;
+  List<String> get expenseList => _expenseCategoryList;
 
   @override
   void onInit() {
@@ -32,13 +32,30 @@ class CategoryController extends GetxController {
   getCategoryList() async {
     List<Category> categoryResponse =
         await categoryOperations.getCategoryList();
-
+    print('category: ${categoryResponse[5].category}');
     // categoryResponse.where((category) => Utils.whichCategory(
     //     category, _expenseCategoryList, _incomeCategoryList));
+    getIncomeExpenseList(categoryResponse);
     update();
   }
 
-  void selectCategory(Object? select){
+  getIncomeExpenseList(List<Category> categoryResponse){
+    List<String> fIncomeList = [];
+    List<String> fExpenseList = [];
+    for(var category in categoryResponse){
+      if(category.isIncome){
+        fIncomeList.add(category.category);
+      }else{
+        fExpenseList.add(category.category);
+      }
+    }
+    _incomeCategoryList.value = fIncomeList;
+    _expenseCategoryList.value = fExpenseList;
+    update();
+  }
+
+
+  void selectCategory(Object? select) {
     _selectedCategory.value = select as String;
     update();
   }
