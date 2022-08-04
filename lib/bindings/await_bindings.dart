508ac05717ad7e5f9a460f47bc6f5a30aaf45953
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-04 17:58:03 +0300

    Update await_bindings.dart

--- lib/bindings/await_bindings.dart
+++ lib/bindings/await_bindings.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
+import 'package:kasa/controllers/amount_controller.dart';
 import 'package:kasa/controllers/bottom_sheet_controller.dart';
 import 'package:kasa/controllers/category_controller.dart';
 
 class AwaitBindings extends Bindings {
   @override
   Future<void> dependencies() async {
+    await Get.putAsync(() async => AmountController(), permanent: true);
     await Get.putAsync(() async => BottomSheetController(), permanent: true);
     await Get.putAsync(() async => CategoryController(), permanent: true);
   }
