69f49769f074e4767ffc11d0cd45c9c921c65826
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-04 17:58:26 +0300

    Update home_page.dart

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:kasa/controllers/amount_controller.dart';
 import 'package:kasa/controllers/bottom_sheet_controller.dart';
-import 'package:kasa/controllers/category_controller.dart';
 import 'package:kasa/core/constrants/app_colors.dart';
-import 'package:kasa/core/constrants/app_keys.dart';
 import 'package:kasa/core/constrants/app_keys_textstyle.dart';
 import 'package:kasa/ui/widgets/home/activity_timeline.dart';
 import 'package:kasa/ui/widgets/home/bottom_sheet_widget.dart';
@@ -12,11 +12,14 @@ import 'package:kasa/ui/widgets/home/month_comparison_card.dart';
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
+  final AmountController _amount = Get.find();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
-        physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
         slivers: [
           SliverAppBar(
             backgroundColor: Colors.transparent,
@@ -32,55 +35,41 @@ class HomePage extends StatelessWidget {
             ),
           ),
           SliverToBoxAdapter(
-            child: Card(
-              margin: EdgeInsets.zero,
-              //color: Colors.red,
-              color: AppColors.bakeryBox,
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15.0),
-                      topRight: Radius.circular(15.0))),
-              child: SingleChildScrollView(
-                physics: const NeverScrollableScrollPhysics(),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: Get.width * 0.05,
-                      vertical: Get.height * 0.04),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text('Son 30 gün'),
-                          Text('+4.342,02',
-                              style: AppKeysTextStyle.currentBalanceStyle)
-                        ],
-                      ),
-                      Column(
-                        //listviewBuilder
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ActivityTimeLine(
-                              amount: -500,
-                              date: '24/07/2022',
-                              name: 'Faturalar'),
-                          ActivityTimeLine(
-                              amount: -400,
-                              date: '25/07/2022',
-                              name: 'Online Alışveriş'),
-                          ActivityTimeLine(
-                              amount: -800,
-                              date: '27/07/2022',
-                              name: 'Mutfak Alışverişi'),
-                          ActivityTimeLine(
-                              amount: -2000, date: '28/07/2022', name: 'kira'),
-                          ActivityTimeLine(
-                              amount: 11000, date: '31/07/2022', name: 'Maaş'),
-                          SizedBox(height: Get.height * 0.1)
-                        ],
-                      )
-                    ],
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: Get.height - 150),
+              child: Card(
+                margin: EdgeInsets.zero,
+                //color: Colors.red,
+                color: AppColors.bakeryBox,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15.0),
+                        topRight: Radius.circular(15.0))),
+                child: SingleChildScrollView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: Get.width * 0.05,
+                        vertical: Get.height * 0.04),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text('Son 30 gün'),
+                            Text('+4.342,02',
+                                style: AppKeysTextStyle.currentBalanceStyle)
+                          ],
+                        ),
+                        Column(
+                          //listviewBuilder
+                          mainAxisSize: MainAxisSize.min,
+                          children: getAmountWidgetList() +
+                              [SizedBox(height: Get.height * 0.1)],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -134,170 +123,32 @@ class HomePage extends StatelessWidget {
         enableDrag: false,
         BottomSheetWidget());
   }
+
+  List<Widget> getAmountWidgetList() {
+    return List.generate(
+        _amount.amountList.length,
+        (index) => ActivityTimeLine(
+            amount: _amount.amountList[index].amount,
+            date: DateFormat.yMd().format(_amount.amountList[index].dateTime),
+            name: _amount.amountList[index].category));
+  }
 }
 
-// SingleChildScrollView(
-//         physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-//         child: Column(
-//           children: [
-//             Expanded(
-//                 flex: 2,
-//                 child: Container(
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Padding(
-//                         padding: const EdgeInsets.symmetric(
-//                             vertical: 15.0, horizontal: 10.0),
-//                         child: TextButton(
-//                             onPressed: () {},
-//                             child: Text(AppKeys.menuText,
-//                                 style: AppKeysTextStyle.menuTextStyle)),
-//                       ),
-//                       SizedBox(height: Get.height * 0.01),
-//                       const Align(
-//                           alignment: Alignment.center,
-//                           child: MonthComparisonCard()), //ListViewBuilder
-//                     ],
-//                   ),
-//                 )),
-//             Expanded(
-//                 flex: 3,
-//                 child: SizedBox(
-//                   width: Get.width,
-//                   child: Card(
-//                     margin: EdgeInsets.zero,
-//                     //color: Colors.red,
-//                     color: AppColors.bakeryBox,
-//                     shape: const RoundedRectangleBorder(
-//                         borderRadius: BorderRadius.only(
-//                             topLeft: Radius.circular(15.0),
-//                             topRight: Radius.circular(15.0))),
-//                     child: Padding(
-//                       padding: EdgeInsets.symmetric(
-//                           horizontal: Get.width * 0.05,
-//                           vertical: Get.height * 0.04),
-//                       child: Column(
-//                         crossAxisAlignment: CrossAxisAlignment.start,
-//                         children: [
-//                           Row(
-//                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                             children: [
-//                               const Text('Son 30 gün'),
-//                               Text('+4.342,02',
-//                                   style: AppKeysTextStyle.currentBalanceStyle)
-//                             ],
-//                           ),
-//                           Expanded(
-//                             child: ListView(
-//                               //listviewBuilder
-//                               shrinkWrap: true,
-//                               physics: const NeverScrollableScrollPhysics(),
-//                               children: [
-//                                 ActivityTimeLine(
-//                                     amount: -500,
-//                                     date: '24/07/2022',
-//                                     name: 'Faturalar'),
-//                                 ActivityTimeLine(
-//                                     amount: -400,
-//                                     date: '25/07/2022',
-//                                     name: 'Online Alışveriş'),
-//                                 ActivityTimeLine(
-//                                     amount: -800,
-//                                     date: '27/07/2022',
-//                                     name: 'Mutfak Alışverişi'),
-//                                 ActivityTimeLine(
-//                                     amount: -2000,
-//                                     date: '28/07/2022',
-//                                     name: 'kira'),
-//                                 ActivityTimeLine(
-//                                     amount: 11000,
-//                                     date: '31/07/2022',
-//                                     name: 'Maaş'),
-//                                 SizedBox(height: Get.height * 0.1)
-//                               ],
-//                             ),
-//                           )
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                 )),
-//           ],
-//         ),
-//       ),
 
 
-
-
-
-
-
-// NestedScrollView(
-//         headerSliverBuilder: (context, innerBoxIsScrolled) {
-//           return [
-//             SliverAppBar(
-//               //title: Text('Başlık'),
-//               backgroundColor: Colors.transparent,
-//               bottom: PreferredSize(
-//                   preferredSize: Size.fromHeight(Get.height * 0.2),
-//                   child: SizedBox(
-//                       height: Get.height * 0.2,
-//                       width: Get.width,
-//                       child: const Align(
-//                           alignment: Alignment.center,
-//                           child: MonthComparisonCard()))),
-//             )
-//           ];
-//         },
-//         body: Card(
-//           margin: EdgeInsets.zero,
-//           //color: Colors.red,
-//           color: AppColors.bakeryBox,
-//           shape: const RoundedRectangleBorder(
-//               borderRadius: BorderRadius.only(
-//                   topLeft: Radius.circular(15.0),
-//                   topRight: Radius.circular(15.0))),
-//           child: SingleChildScrollView(
-//             physics: const NeverScrollableScrollPhysics(),
-//             child: Padding(
-//               padding: EdgeInsets.symmetric(
-//                   horizontal: Get.width * 0.05, vertical: Get.height * 0.04),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                     children: [
-//                       const Text('Son 30 gün'),
-//                       Text('+4.342,02',
-//                           style: AppKeysTextStyle.currentBalanceStyle)
-//                     ],
-//                   ),
-//                   Column(
-//                     //listviewBuilder
-//                     mainAxisSize: MainAxisSize.min,
-//                     children: [
-//                       ActivityTimeLine(
-//                           amount: -500, date: '24/07/2022', name: 'Faturalar'),
-//                       ActivityTimeLine(
-//                           amount: -400,
-//                           date: '25/07/2022',
-//                           name: 'Online Alışveriş'),
-//                       ActivityTimeLine(
-//                           amount: -800,
-//                           date: '27/07/2022',
-//                           name: 'Mutfak Alışverişi'),
-//                       ActivityTimeLine(
-//                           amount: -2000, date: '28/07/2022', name: 'kira'),
-//                       ActivityTimeLine(
-//                           amount: 11000, date: '31/07/2022', name: 'Maaş'),
-//                       SizedBox(height: Get.height * 0.1)
-//                     ],
-//                   )
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
+// ActivityTimeLine(
+//                               amount: -500,
+//                               date: '24/07/2022',
+//                               name: 'Faturalar'),
+//                           ActivityTimeLine(
+//                               amount: -400,
+//                               date: '25/07/2022',
+//                               name: 'Online Alışveriş'),
+//                           ActivityTimeLine(
+//                               amount: -800,
+//                               date: '27/07/2022',
+//                               name: 'Mutfak Alışverişi'),
+//                           ActivityTimeLine(
+//                               amount: -2000, date: '28/07/2022', name: 'kira'),
+//                           ActivityTimeLine(
+//                               amount: 11000, date: '31/07/2022', name: 'Maaş')
