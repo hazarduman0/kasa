3f94b5e25528cb1a8c653a46b569f2d579b55214
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-04 17:58:22 +0300

    Update amount_provider.dart

--- lib/data/provider/amount_provider.dart
+++ lib/data/provider/amount_provider.dart
@@ -1,4 +1,5 @@
 import 'package:kasa/data/database.dart';
+import 'package:kasa/data/models/amount.dart';
 
 class AmountOperations {
   DatabaseRepository dbRepository = DatabaseRepository.instance;
@@ -7,5 +8,25 @@ class AmountOperations {
   //read
   //update
   //delete
+
+  Future<Amount> createAmount(Amount amount) async {
+    final db = await dbRepository.database;
+
+    final id = await db.insert(tableAmount, amount.toJson());
+    return amount.copy(id: id);
+  }
+
+  Future<List<Amount>> getAmountList () async {
+    final db = await dbRepository.database;
+
+    const orderBy = '${AmountFields.category} ASC';
+
+    final result = await db.query(
+      tableAmount,
+      orderBy: orderBy,
+    );
+
+    return result.map((json) => Amount.fromJson(json)).toList();
+  }
 
 }
