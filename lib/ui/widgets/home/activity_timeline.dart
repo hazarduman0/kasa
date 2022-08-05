4b358bdbb0152310b00a91059f070ce8399bb4ca
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-05 18:49:46 +0300

    Update activity_timeline.dart

--- lib/ui/widgets/home/activity_timeline.dart
+++ lib/ui/widgets/home/activity_timeline.dart
@@ -2,49 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:kasa/controllers/input_controller.dart';
 import 'package:kasa/core/constrants/app_colors.dart';
 import 'package:kasa/core/constrants/app_keys_textstyle.dart';
+import 'package:kasa/data/models/amount.dart';
+import 'package:kasa/ui/screens/amount_detail.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
 class ActivityTimeLine extends StatelessWidget {
   ActivityTimeLine(
-      {Key? key, required this.amount, required this.date, required this.name})
+      {Key? key,
+      required this.amount,
+      this.isFirst = false,
+      this.isLast = false})
       : super(key: key);
 
-  double amount;
-  String name;
-  String date;
-  final double _thickness = 2.0;
+  Amount amount;
+  // double amount;
+  // String name;
+  // String date;
+  bool isLast;
+  bool isFirst;
+  final double _thickness = 1.0;
+
+  final InputController inputController = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return TimelineTile(
-      alignment: TimelineAlign.center,
-      startChild: SizedBox(
-        height: Get.height * 0.1,
-        width: Get.width * 0.4,
-        child: _incomeText(),
-      ),
-      indicatorStyle: IndicatorStyle(
-          color: amount > 0 ? AppColors.limonana : AppColors.silkenRuby,
-          height: 15.0,
-          width: 15.0),
-      beforeLineStyle: LineStyle(
-          color: amount > 0 ? AppColors.limonana : AppColors.silkenRuby,
-          thickness: _thickness),
-      afterLineStyle: LineStyle(
-          color: amount > 0 ? AppColors.limonana : AppColors.silkenRuby,
-          thickness: _thickness),
-      endChild: SizedBox(
-        height: Get.height * 0.1,
-        width: Get.width * 0.4,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(name, style: AppKeysTextStyle.timeLineNameTextStyle),
-            Text(date)
-          ],
+    return GestureDetector(
+      onTap: () {
+        amount.amount > 0
+            ? inputController.isRemoveFunc(false)
+            : inputController.isRemoveFunc(true);
+        Get.to(() => AmountDetailPage(amount: amount));
+      },
+      child: TimelineTile(
+        alignment: TimelineAlign.center,
+        isLast: isLast,
+        isFirst: isFirst,
+        startChild: SizedBox(
+          height: Get.height * 0.1,
+          width: Get.width * 0.4,
+          child: _incomeText(),
+        ),
+        indicatorStyle: IndicatorStyle(
+            padding: const EdgeInsets.all(4.0),
+            color:
+                amount.amount > 0 ? AppColors.limonana : AppColors.silkenRuby,
+            height: 10.0,
+            width: 10.0),
+        beforeLineStyle: LineStyle(
+            color:
+                amount.amount > 0 ? AppColors.limonana : AppColors.silkenRuby,
+            thickness: _thickness),
+        afterLineStyle: LineStyle(
+            color:
+                amount.amount > 0 ? AppColors.limonana : AppColors.silkenRuby,
+            thickness: _thickness),
+        endChild: SizedBox(
+          height: Get.height * 0.1,
+          width: Get.width * 0.4,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(amount.category,
+                  style: AppKeysTextStyle.timeLineNameTextStyle),
+              Text(DateFormat.yMd().format(amount.dateTime))
+            ],
+          ),
         ),
       ),
     );
@@ -53,8 +80,8 @@ class ActivityTimeLine extends StatelessWidget {
   Align _incomeText() {
     return Align(
       alignment: Alignment.center,
-      child: Text(amount > 0 ? '+ $amount' : '$amount',
-          style: amount > 0
+      child: Text(amount.amount > 0 ? '+ ${amount.amount}' : '${amount.amount}',
+          style: amount.amount > 0
               ? AppKeysTextStyle.currentBalanceStyle
               : AppKeysTextStyle.currentBalanceStyle
                   .copyWith(color: AppColors.silkenRuby)),
