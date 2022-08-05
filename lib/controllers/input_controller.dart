8c2cf2b62ff7e4c5ca6d8aed650e0d2c9dd2af88
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-05 18:49:30 +0300

    Create input_controller.dart

---
+++ lib/controllers/input_controller.dart
@@ -0,0 +1,57 @@
+import 'package:get/get.dart';
+
+class InputController extends GetxController {
+  //final RxBool _isFixedChoosen = false.obs;
+  final RxBool _isRemove = false.obs;
+  final RxString _selectedCategoryText = ''.obs;
+  final RxString _descriptionText = ''.obs;
+  final RxDouble _amountDouble = 0.0.obs;
+
+  //bool get isFixedChoosen => _isFixedChoosen.value;
+  bool get isRemove => _isRemove.value;
+  String get selectedCategoryText => _selectedCategoryText.value;
+  String get descriptionText => _descriptionText.value;
+  double get amountDouble => _amountDouble.value;
+
+  setCategoryText(Object? selectedText) {
+    if (selectedText != null) {
+      _selectedCategoryText.value = selectedText as String;
+      update();
+    }
+  }
+
+  setDescriptionText(String? descriptionFormText) {
+    if (descriptionFormText!.isNotEmpty) {
+      _descriptionText.value = descriptionFormText;
+      update();
+    }
+  }
+
+  setAmount(String? formAmount) {
+    if (formAmount!.isNotEmpty) {
+      _amountDouble.value = double.tryParse(formAmount)!;
+      print(_amountDouble);
+      update();
+    }
+  }
+
+  void selectCategory(Object? select) {
+    _selectedCategoryText.value = select as String;
+    update();
+  }
+
+  // fixedButtonFunc() {
+  //   _isFixedChoosen.value = true;
+  //   update();
+  // }
+
+  // extraButtonFunc() {
+  //   _isFixedChoosen.value = false;
+  //   update();
+  // }
+
+  isRemoveFunc(bool isRemoveF) {
+    _isRemove.value = isRemoveF;
+    update();
+  }
+}
