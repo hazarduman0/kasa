198a0b44b4771a6c39cad93bd18c7f022b253582
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-05 18:49:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(elevation: 0.0)
       ),
       home: HomePage(),
     );
