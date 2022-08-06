aff17c84ad1d04b28dda15af339075d723650d48
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-06 20:55:38 +0300

    Update amount_controller.dart

--- lib/controllers/amount_controller.dart
+++ lib/controllers/amount_controller.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:kasa/core/utils.dart';
 import 'package:kasa/data/models/amount.dart';
 import 'package:kasa/data/provider/amount_provider.dart';
 
@@ -7,23 +8,75 @@ class AmountController extends GetxController {
 
   final RxList<Amount> _amountList = <Amount>[].obs;
   final RxBool _isLoading = false.obs;
+  final RxString _selectedRange = 'Son 15 gün'.obs;
+  final RxBool _rangeCrossFadeBool = false.obs;
+  final RxDouble _netAmount = 0.0.obs;
 
   List<Amount> get amountList => _amountList;
+  String get selectedRange => _selectedRange.value;
+  double get netAmount => _netAmount.value;
   bool get isLoading => _isLoading.value;
+  bool get rangeCrossFadeBool => _rangeCrossFadeBool.value;
 
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
     getAmountList();
+    //print('deneme: ${amountOperations.deneme()}');
   }
 
+  // getAmountList() async {
+  //   _isLoading.value = true;
+  //   update();
+  //   List<Amount> amountResponse = await amountOperations.deneme();
+  //   _amountList.value = amountResponse;
+  //   _isLoading.value = false;
+  //   update();
+  // }
+
   getAmountList() async {
+    String now = '${DateTime.now()}';
+    String input =
+        '${(DateTime.now()).subtract(Utils.timeAgoFunc(_selectedRange.value))}';
+
+    String timeago = '${input.substring(0, 10)}T${input.substring(11)}';
+    String currentTime = '${now.substring(0, 10)}T${now.substring(11)}}';
     _isLoading.value = true;
     update();
-    List<Amount> amountResponse = await amountOperations.getAmountList();
+    List<Amount> amountResponse = await amountOperations.getDesignatedList(
+        timeago: timeago, date: currentTime);
+    setNetAmount(amountResponse);
     _amountList.value = amountResponse;
     _isLoading.value = false;
+    update();
+  }
+
+  setNetAmount(List<Amount> amountList) {
+    double net = 0.0;
+    for (var amount in amountList) {
+      net = net + (amount.amount);
+    }
+    _netAmount.value = net;
+    update();
+  }
+
+  // getAmountList() async {
+  //   _isLoading.value = true;
+  //   update();
+  //   List<Amount> amountResponse = await amountOperations.getAmountList();
+  //   _amountList.value = amountResponse;
+  //   _isLoading.value = false;
+  //   update();
+  // }
+
+  setSelectedRange(String range) {
+    _selectedRange.value = range;
+    update();
+  }
+
+  setRangeCrossFadeBool(bool crossFadeBool) {
+    _rangeCrossFadeBool.value = crossFadeBool;
     update();
   }
 }
