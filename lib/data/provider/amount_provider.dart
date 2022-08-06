ab06b88224161d092a592c2d0ae9bccd30310d87
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-06 20:55:46 +0300

    Update amount_provider.dart

--- lib/data/provider/amount_provider.dart
+++ lib/data/provider/amount_provider.dart
@@ -16,17 +16,64 @@ class AmountOperations {
     return amount.copy(id: id);
   }
 
-  Future<List<Amount>> getAmountList () async {
+  Future<int> updateAmount(Amount amount) async {
+    final db = await dbRepository.database;
+
+    return db.update(
+      tableAmount,
+      amount.toJson(),
+      where: '${AmountFields.id} = ?',
+      whereArgs: [amount.id],
+    );
+  }
+
+  Future<int> deleteAmount(int? id) async {
+    final db = await dbRepository.database;
+
+    return await db.delete(
+      tableAmount,
+      where: '${AmountFields.id} = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // Future<List<Amount>> deneme() async {
+  //   final db = await dbRepository.database;
+
+  //   // const orderBy = '${AmountFields.dateTime} DESC';
+
+  //   // const where = '2022-08-05 21:30:48.913092 ';
+
+  //   final result = await db.rawQuery(
+  //       '''SELECT * FROM $tableAmount WHERE ${AmountFields.dateTime} BETWEEN '2022-08-05T21:30:48.913092' AND '${DateTime.now()}' ORDER BY ${AmountFields.dateTime} DESC ''');
+
+  //   return result.map((json) => Amount.fromJson(json)).toList();
+  // }
+
+  Future<List<Amount>> getDesignatedList(
+      {required String timeago, required String date}) async {
     final db = await dbRepository.database;
 
     const orderBy = '${AmountFields.dateTime} DESC';
 
-    final result = await db.query(
-      tableAmount,
-      orderBy: orderBy,
-    );
+    const where = AmountFields.dateTime;
+
+    final result = await db.rawQuery(
+        '''SELECT * FROM $tableAmount WHERE $where BETWEEN '$timeago' AND '$date' ORDER BY $orderBy ''');
 
     return result.map((json) => Amount.fromJson(json)).toList();
   }
 
+  // Future<List<Amount>> getAmountList() async {
+  //   final db = await dbRepository.database;
+
+  //   const orderBy = '${AmountFields.dateTime} DESC';
+
+  //   final result = await db.query(
+  //     tableAmount,
+  //     orderBy: orderBy,
+  //   );
+
+  //   return result.map((json) => Amount.fromJson(json)).toList();
+  // }
 }
