6ed3fc3509856cf0ff6ac4a63c820f77b44dbd90
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-06 20:55:43 +0300

    Update utils.dart

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,12 +1,25 @@
-import 'package:kasa/data/models/category.dart';
-
 class Utils {
-  static whichCategory(
-      Category category, List<String> expenseList, List<String> incomeList) {
-    if (category.isIncome) {
-      expenseList.add(category.category);
-    } else {
-      incomeList.add(category.category);
+
+  static Duration timeAgoFunc(String timeago){
+    Duration duration = const Duration(days: 15);
+    if(timeago == 'Son 12 saat'){
+      duration = const Duration(hours: 12);
     }
+    if(timeago == 'Son 24 saat'){
+      duration = const Duration(hours: 24);
+    }
+    if(timeago == 'Son hafta'){
+      duration = const Duration(days: 7);
+    }
+    if(timeago == 'Son 30 gün'){
+      duration = const Duration(days: 30);
+    }
+    if(timeago == 'Son 60 gün'){
+      duration = const Duration(days: 60);
+    }
+    if(timeago == 'Son 90 gün'){
+      duration = const Duration(days: 90);
+    }
+    return duration;
   }
 }
