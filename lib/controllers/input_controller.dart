2dc93e9a3e9aeacaa2cc70b59984cc0bef0a4134
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-08 17:53:39 +0300

    Update input_controller.dart

--- lib/controllers/input_controller.dart
+++ lib/controllers/input_controller.dart
@@ -1,12 +1,23 @@
 import 'package:get/get.dart';
+import 'package:kasa/data/models/amount.dart';
+import 'package:kasa/data/provider/amount_provider.dart';
 
 class InputController extends GetxController {
+  AmountOperations amountOperations = AmountOperations();
+
   //final RxBool _isFixedChoosen = false.obs;
   final RxBool _isRemove = false.obs;
   final RxBool _editStackBool = false.obs;
+  final RxBool _isFixed = false.obs;
   final RxString _selectedCategoryText = ''.obs;
   final RxString _descriptionText = ''.obs;
   final RxDouble _amountDouble = 0.0.obs;
+  final RxInt _dayPeriod = 0.obs;
+  final RxInt _hourPeriod = 0.obs;
+  final RxInt _minutePeriod = 0.obs;
+  final Rx<DateTime?> _lastPeriodDate = Rxn();
+  final Rx<Duration?> _frequency = Rxn();
+  //final RxBool _isValidTimePeriod = true.obs;
 
   //bool get isFixedChoosen => _isFixedChoosen.value;
   bool get editBool => _editStackBool.value;
@@ -14,6 +25,13 @@ class InputController extends GetxController {
   String get selectedCategoryText => _selectedCategoryText.value;
   String get descriptionText => _descriptionText.value;
   double get amountDouble => _amountDouble.value;
+  int get dayPeriod => _dayPeriod.value;
+  int get hourPeriod => _hourPeriod.value;
+  int get minutePeriod => _minutePeriod.value;
+  bool get isFixed => _isFixed.value;
+  DateTime? get lastPeriodDate => _lastPeriodDate.value;
+  Duration? get frequency => _frequency.value;
+  //bool get isValidTimePeriod => _isValidTimePeriod.value;
 
   setCategoryText(Object? selectedText) {
     if (selectedText != null) {
@@ -21,6 +39,64 @@ class InputController extends GetxController {
       update();
     }
   }
+
+  setDayPeriod(String? value) {
+    if (value != null) {
+      _dayPeriod.value = int.parse(value);
+      update();
+    }
+  }
+
+  setHourPeriod(String? value) {
+    if (value != null) {
+      _hourPeriod.value = int.parse(value);
+      update();
+    }
+  }
+
+  setMinutePeriod(String? value) {
+    if (value != null) {
+      _minutePeriod.value = int.parse(value);
+      update();
+    }
+  }
+
+  setLastPeriodDate(DateTime? value) {
+    _lastPeriodDate.value = value;
+    print('controller: ${_lastPeriodDate.value}');
+    update();
+  }
+
+  setFrequency() {
+    _frequency.value = Duration(
+        days: _dayPeriod.value,
+        hours: _hourPeriod.value,
+        minutes: _minutePeriod.value);
+    print(_frequency.value);
+    print(_dayPeriod.value);
+    update();
+  }
+
+  ////
+  insertDataByFrequency(Amount amount) async {
+    //List<Amount> amountList = [];
+    final timeDifference =
+        _lastPeriodDate.value!.difference(DateTime.now()).inMinutes;
+    // print('timeDifference: $timeDifference');
+    // print('_frequency.value!.inMinutes : ${_frequency.value!.inMinutes}');
+    final loopInt = timeDifference ~/ _frequency.value!.inMinutes;
+    var tempDate = DateTime.now();
+    //print('loopInt : $loopInt');
+    for (int i = 0; i <= loopInt; i++) {
+      await amountOperations.createAmount(amount.copy(dateTime: tempDate));
+      tempDate = tempDate.subtract(Duration(minutes: timeDifference));
+    }
+  }
+
+  // setValidTimePeriod(bool value){
+  //   _isValidTimePeriod.value = value;
+  //   update();
+  // }
 
   setEditStackBool(bool editStack) {
     _editStackBool.value = editStack;
@@ -42,6 +118,11 @@ class InputController extends GetxController {
 
   void selectCategory(Object? select) {
     _selectedCategoryText.value = select as String;
+    update();
+  }
+
+  setIsFixed(bool value) {
+    _isFixed.value = value;
     update();
   }
 
