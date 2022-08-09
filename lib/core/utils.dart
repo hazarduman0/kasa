53bb6423d0192275830fec8be4e1f9fa46b08eb0
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-09 18:00:06 +0300

    Update utils.dart

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,25 +1,44 @@
 class Utils {
-
-  static Duration timeAgoFunc(String timeago){
+  static Duration timeAgoFunc(String timeago) {
     Duration duration = const Duration(days: 15);
-    if(timeago == 'Son 12 saat'){
+    if (timeago == 'Son 12 saat') {
       duration = const Duration(hours: 12);
     }
-    if(timeago == 'Son 24 saat'){
+    if (timeago == 'Son 24 saat') {
       duration = const Duration(hours: 24);
     }
-    if(timeago == 'Son hafta'){
+    if (timeago == 'Son hafta') {
       duration = const Duration(days: 7);
     }
-    if(timeago == 'Son 30 gün'){
+    if (timeago == 'Son 30 gün') {
       duration = const Duration(days: 30);
     }
-    if(timeago == 'Son 60 gün'){
+    if (timeago == 'Son 60 gün') {
       duration = const Duration(days: 60);
     }
-    if(timeago == 'Son 90 gün'){
+    if (timeago == 'Son 90 gün') {
       duration = const Duration(days: 90);
     }
     return duration;
+  }
+
+  static int byMinutes(String choosenPeriod) {
+    int inMinutes = const Duration(days: 30).inMinutes;
+    if (choosenPeriod == '60 gün') {
+      inMinutes = const Duration(days: 60).inMinutes;
+    }
+    if (choosenPeriod == '90 gün') {
+      inMinutes = const Duration(days: 90).inMinutes;
+    }
+    if (choosenPeriod == '1 hafta') {
+      inMinutes = const Duration(days: 7).inMinutes;
+    }
+    if (choosenPeriod == '2 hafta') {
+      inMinutes = const Duration(days: 14).inMinutes;
+    }
+    if (choosenPeriod == '24 saat') {
+      inMinutes = const Duration(days: 1).inMinutes;
+    }
+    return inMinutes;
   }
 }
