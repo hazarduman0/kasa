f9eba5c4f63a6bfb243455a2ed087000274dbd1d
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-10 00:14:38 +0300

    Update await_bindings.dart

--- lib/bindings/await_bindings.dart
+++ lib/bindings/await_bindings.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:get/get.dart';
 import 'package:kasa/controllers/amount_controller.dart';
+import 'package:kasa/controllers/chart_controller.dart';
 import 'package:kasa/controllers/input_controller.dart';
 import 'package:kasa/controllers/category_controller.dart';
 
@@ -10,11 +11,8 @@ class AwaitBindings extends Bindings {
   Future<void> dependencies() async {
     
     await Get.putAsync(() async => CategoryController(), permanent: true);
-    log("1");
-    log("2");
-    log("3");
     await Get.putAsync(() async => AmountController(), permanent: true);
-    log("4");
     await Get.putAsync(() async => InputController(), permanent: true);
+    //await Get.putAsync(() async => ChartController(), permanent: true);
   }
 }
