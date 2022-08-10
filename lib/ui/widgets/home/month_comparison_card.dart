2f73503471c646352068e4f7a64b581e96b9875a
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-10 22:12:37 +0300

    Update month_comparison_card.dart

--- lib/ui/widgets/home/month_comparison_card.dart
+++ lib/ui/widgets/home/month_comparison_card.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,7 +10,13 @@ class MonthComparisonCard extends StatelessWidget {
     return SizedBox(
       height: Get.height * 0.21,
       width: Get.width * 0.8,
-      child: const Card(),
+      child: const Card(
+        child: Center(
+          child: BarChart(
+            
+          ),
+        ),
+      ),
     );
   }
 }
