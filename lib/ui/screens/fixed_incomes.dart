3e41ea103e5f5ab534e50d4fbb5f61c0537d3716
Hazar Duman <93342169+hazarduman0@users.noreply.github.com>
2022-08-10 22:12:18 +0300

    Create fixed_incomes.dart

---
+++ lib/ui/screens/fixed_incomes.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:kasa/controllers/income_expense_controller.dart';
+import 'package:kasa/core/constrants/app_keys_textstyle.dart';
+import 'package:kasa/ui/widgets/income/income_listtile.dart';
+
+class FixedIncomes extends StatelessWidget {
+  const FixedIncomes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBarBuild(),
+      body: SizedBox(
+        height: Get.height,
+        width: Get.width,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: Get.width * 0.05, vertical: Get.height * 0.05),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                 Text('Tüm Sabit Gelirler', style: AppKeysTextStyle.incomeExpenseAppBarTextStyle.copyWith(fontSize: Get.width * 0.05),),
+                SizedBox(height: Get.height * 0.05),
+                GetBuilder<IncomeExpenseController>(
+                  builder: (incomeExpense) {
+                    return SizedBox(
+                      height: Get.height * 0.8,
+                      width: Get.width,
+                      child: ListView.builder(
+                        physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                      itemCount: incomeExpense.fixedIncomeList.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            IncomeListTile(amount: incomeExpense.fixedIncomeList[index]),
+                            const Divider(),
+                          ],
+                        );
+                      },));
+                  }
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar _appBarBuild() {
+    return AppBar(
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: const Icon(Icons.arrow_back_ios_outlined, color: Colors.black)),
+    );
+  }
+}
